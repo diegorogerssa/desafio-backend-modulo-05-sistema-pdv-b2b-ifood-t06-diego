d3f78d66194e590015a674e036748a3902684aa8
anjosdelacerda <anjosdelacerda@gmail.com>
2023-10-23 20:47:58 -0300

    :fix: correção nas importações erradas do knex

--- src/database/knex.js
+++ src/database/knex.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const localConfig = {
+const localConfig = require("knex")({
   client: 'pg',
   connection: {
     host: process.env.POSTGRES_LOCAL_HOST,
@@ -8,10 +8,10 @@ const localConfig = {
     password: process.env.POSTGRES_LOCAL_PASS,
     port: process.env.POSTGRES_LOCAL_PORT,
     database: process.env.POSTGRES_LOCAL_DB,
-  },
-};
+  }
+});
 
-const elephantConfig = {
+const elephantConfig = require("knex")({
   client: 'pg',
   connection: {
     host: process.env.POSTGRES_HOST,
@@ -20,9 +20,9 @@ const elephantConfig = {
     port: process.env.POSTGRES_PORT,
     database: process.env.POSTGRES_DB,
   },
-};
+});
 
-const config = process.env.NODE_ENV === 'production' ? elephantConfig : localConfig;
-const knex = config;
+const knex = process.env.NODE_ENV === 'production' ? elephantConfig : localConfig;
+
 
 module.exports = knex;
