bc2f0cfa2c5f55b0d8066c03d51a85b8fadbee02
anjosdelacerda <anjosdelacerda@gmail.com>
2023-10-23 18:31:45 -0300

    :fix: alterações de credenciais .env para deploy/configurações no knex para deploy

--- src/database/knex.js
+++ src/database/knex.js
@@ -1,14 +1,26 @@
 require('dotenv').config();
 
-const knex = require('knex')({
+const localConfig = require('knex')({
   client: 'pg',
   connection: {
-    host: process.env.POSTGRES_HOST,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASS,
-    port: process.env.POSTGRES_PORT,
-    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_LOCAL_HOST,
+    user: process.env.POSTGRES_LOCAL_USER,
+    password: process.env.POSTGRES_LOCAL_PASS,
+    port: process.env.POSTGRES_LOCAL_PORT,
+    database: process.env.POSTGRES_LOCAL_DB,
+
   },
 });
+
+const renderConfig = {
+  client: 'pg',
+  connection: process.env.DATABASE_URL_RENDER, 
+  ssl: { rejectUnauthorized: false }, 
+};
+
+const config = process.env.NODE_ENV === 'production' ? renderConfig : localConfig;
+
+const knex = config
+
 
 module.exports = knex;
