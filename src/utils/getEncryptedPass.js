8853f54692472973de82793056eb78d68a76a557
anjosdelacerda <anjosdelacerda@gmail.com>
2023-10-25 22:30:34 -0300

    :fix: exclusão da pasta utils/transferência do bcrypt.compare para a service de login

--- src/utils/getEncryptedPass.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcrypt');
-
-const comparePasswords = async (inputPassword, storedPassword) => {
-  const response = await bcrypt.compare(inputPassword, storedPassword);
-
-  return response
-};
-
-module.exports = comparePasswords;
