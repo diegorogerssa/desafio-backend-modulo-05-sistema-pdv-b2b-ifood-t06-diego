ab8c1eab7d9fad25f4744b16950c95b22f11703f
FoxCheeze <iagocarvalhodm@gmail.com>
2023-10-28 11:49:17 -0300

    chore: descricao da categoria ao listar produtos

--- src/services/product/listProductService.js
+++ src/services/product/listProductService.js
@@ -1,7 +1,18 @@
-const { listProductModel } = require('../../models');
+const {
+  listProductModel,
+  getCategoryByIDModel
+} = require('../../models');
 
 const listProductService = async (category_id) => {
   const products = await listProductModel();
+
+  for (const product of products) {
+    const categoria_id = product.categoria_id
+    const { descricao } = await getCategoryByIDModel(categoria_id);
+
+    product.categoria_descricao = descricao;
+  }
+
   return products;
 };
 
