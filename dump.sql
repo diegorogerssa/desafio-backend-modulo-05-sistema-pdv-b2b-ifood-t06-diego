c1bb8838db576d38fa026fae576dc55f6eb1fd74
anjosdelacerda <anjosdelacerda@gmail.com>
2023-10-28 04:07:21 -0300

    :ft: adicionando schema de tabela clientes

--- dump.sql
+++ dump.sql
@@ -22,3 +22,16 @@ VALUES
   ('Moda'),
   ('Bebê'),
   ('Games');
+
+  CREATE TABLE clientes(
+  id SERIAL PRIMARY KEY,
+  nome VARCHAR(150) NOT NULL,
+  email VARCHAR(150) NOT NULL UNIQUE,
+  cpf CHAR(11) NOT NULL UNIQUE,
+  cep char(8),
+  rua VARCHAR(150),
+  numero VARCHAR(50),
+  bairro VARCHAR(150),
+  cidade VARCHAR(150),
+  estado VARCHAR(3)
+);
