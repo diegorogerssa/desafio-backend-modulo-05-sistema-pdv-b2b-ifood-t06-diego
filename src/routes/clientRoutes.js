9d32da08d34c0144feeb2235784357ed3863e9c8
anjosdelacerda <anjosdelacerda@gmail.com>
2023-10-28 22:42:56 -0300

    :ft: acrescentando middleware de autenticação de usuario nas rotas de clientes

--- src/routes/clientRoutes.js
+++ src/routes/clientRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { validateData } = require('../middlewares');
+const { validateData, authenticationToken } = require('../middlewares');
 const { schemaCreateClient, schemaUpdateClient } = require('../middlewares/schemas');
 const { createClientController, updateClientController } = require('../controllers');
 
 const client = express.Router();
 
-client.post('/clientes', validateData(schemaCreateClient), createClientController);
-client.put('/clientes/:id', validateData(schemaUpdateClient), updateClientController);
+client.post('/clientes', authenticationToken, validateData(schemaCreateClient), createClientController);
+client.put('/clientes/:id', authenticationToken, validateData(schemaUpdateClient), updateClientController);
 
 module.exports = client;
