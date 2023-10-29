505c49265e2f2204502c6a58a7f853a04a03fd4b
diegorogerssa <rogersmhdg@gmail.com>
2023-10-29 20:42:54 -0300

    refactor: corrige, catch do controller

--- src/controllers/product/detailProductController.js
+++ src/controllers/product/detailProductController.js
@@ -1,12 +1,12 @@
 const { detailProductService } = require('../../services');
 
-const detailProductController = async (req, res) => {
+const detailProductController = async (req, res, next) => {
   const { id } = req.params;
   try {
     const detailProduct = await detailProductService(id);
     return res.status(200).json(detailProduct);
   } catch (error) {
-    return res.status(400).json({ message: error.message });
+    return next(error)
   }
 };
 
